5ed3493539713e178d66275d35acf62a95aec50d
Vimal-Mathew16 <115931914+Vimal-Mathew16@users.noreply.github.com>
2024-06-12 21:25:29 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{'id': task.id, 'title': task.title, 'completed': task.completed} for task in tasks])
+
+@app.route('/api/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], completed=data['completed'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'id': new_task.id, 'title': new_task.title, 'completed': new_task.completed})
+
+@app.route('/api/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return '', 204
+
+@app.route('/api/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = Task.query.get_or_404(id)
+    data = request.get_json()
+    task.title = data['title']
+    task.completed = data['completed']
+    db.session.commit()
+    return jsonify({'id': task.id, 'title': task.title, 'completed': task.completed})
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
